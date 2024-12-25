ff56d8ee3f4f9fa7ed9f78edce8b71578427d872
giangduc24 <giangduc24@gmail.com>
2024-12-25 09:52:13 +0700

    F

---
+++ UART/Flash.h
@@ -0,0 +1,19 @@
+/*
+ * Flash.h
+ *
+ *  Created on: Dec 25, 2024
+ *      Author: sh124
+ */
+
+#ifndef FLASH_H_
+#define FLASH_H_
+#include "MKE16Z4.h"
+
+void FLASH_Write(uint32_t Address, uint32_t Data);
+void FLASH_Erase(uint32_t Address);
+void Flash_EraseSector(uint32_t SectorNum);
+uint32_t Flash_Read(uint32_t Address);
+
+
+
+#endif /* FLASH_H_ */
